d48f70e99db913ce95127edb92fd0243e21431bd
nuitciel99 <nuitciel99@naver.com>
2023-02-14 00:05:54 +0900

    주석처리추가

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -4,10 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+// 컨트롤러 어노테이션을 통해 클라이언트(사용자)의 요청을 받아주는 역할
 @Controller
 public class HelloController {
 
+    // GetMapping은 사용자가 get요청을 했을경우 받아주는 역할
+    // post요청 같은 경우에는 PostMapping을 사용
+    // GetMapping, PostMapping 뒤에 받아주는 url을 넣어 경로를 잡아줌
     @GetMapping("/hello-spring")
+    // ResponseBody가 있으면 사용자에게 data를 내려주고
+    // ResponseBody가 없으면 사용자에게 화면을 내려준다는 뜻
     @ResponseBody
     public String helloSpring(){
         return "hello-world-spring";
